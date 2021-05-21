a3ba3708ee345d440880ef1700428b47e67d6f28
EvaldoRC <evaldorcardoso@gmail.com>
2021-05-21 17:29:17 -0300

    CRUD completo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
-        //dd($posts);
+        //$posts = Post::orderBy('id', 'DESC')->paginate();        
+        $posts = Post::latest()->paginate();        
         return view('admin.posts.index', compact('posts'));
     }
 
@@ -22,6 +22,68 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $post = Post::create($request->all());
-        return redirect()->route('posts.index');
+        //dd($post);
+        return redirect()
+                ->route('posts.index')
+                ->with('message', 'Post criado com sucesso!');
+    }
+
+    public function show($id)
+    {
+        //$post = Post::where('id',$id)->first();
+        if(!$post = Post::find($id)){
+            return redirect()->route('posts.index');
+        }
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        if(!$post = Post::find($id)){
+            return redirect()->route('posts.index');
+        }
+        $post->delete();
+        
+        return redirect()
+                ->route('posts.index')
+                ->with('message', 'Post deletado com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        //$post = Post::where('id',$id)->first();
+        if(!$post = Post::find($id)){
+            return redirect()->back();
+        }
+
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        //$post = Post::where('id',$id)->first();
+        if(!$post = Post::find($id)){
+            return redirect()->back();
+        }
+        $post->update($request->all());
+
+        return redirect()
+                ->route('posts.index')
+                ->with('message', 'Post atualizado com sucesso!');
+    }
+
+    public function search(Request $request)
+    {
+        //dd("pesquisando por {$request->search}");
+        //$posts = Post::where('title', '=', "%{$request->search}%")
+        //                ->orWhere('content', 'LIKE', "%{$request->search}%")
+        //                ->toSql();
+        //dd($posts);
+        $filters = $request->except('_token');
+        $posts = Post::where('title', '=', "{$request->search}")
+                        ->orWhere('content', 'LIKE', "%{$request->search}%")
+                        ->paginate();
+        return view('admin.posts.index', compact('posts', 'filters'));
     }
 }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,7 +1,11 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Criando um Post')
+    
+@section('content')
 <h1>Cadastrar novo post</h1>
+
 <form action="{{ route('posts.store') }}" method="POST">
-    @csrf
-    <input type="text" name="title" id="title" placeholder="Título">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo"></textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.posts._partials.form')
 </form>
+@endsection
